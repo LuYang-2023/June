971edbf7403e3e9504ecb7dac2e4b628991f6dbd
我运气超好 <yj20220275@stud.tjut.edu.cn>
2024-06-14 09:21:03 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,204 @@
+import argparse
+import time
+import datetime
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from net import Net
+from dataset import *
+import matplotlib.pyplot as plt
+from metrics import *
+import numpy as np
+import os
+from torch.optim import Adagrad
+from loss import AverageMeter
+from tqdm import tqdm
+import torch.utils.data as Data
+import torchvision.transforms as transforms
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
+parser = argparse.ArgumentParser(description="PyTorch BasicIRSTD train")
+parser.add_argument("--model_names", default=['SCTransNet'], nargs='+',
+                    help="model_name: 'ACM', 'ALCNet', 'DNANet', 'ISNet', 'UIUNet', 'RDIAN', 'ISTDU-Net', 'U-Net', 'RISTDnet'")
+parser.add_argument("--dataset_names", default=['WideIRSTD'], nargs='+',
+                    help="dataset_name: 'NUAA-SIRST', 'NUDT-SIRST', 'IRSTD-1K', 'SIRST3', 'NUDT-SIRST-Sea', 'IRDST-real'")
+parser.add_argument("--img_norm_cfg", default=None, type=dict,
+                    help="specific a img_norm_cfg, default=None (using img_norm_cfg values of each dataset)")
+parser.add_argument("--img_norm_cfg_mean", default=None, type=float,
+                    help="specific a mean value img_norm_cfg, default=None (using img_norm_cfg values of each dataset)")
+parser.add_argument("--img_norm_cfg_std", default=None, type=float,
+                    help="specific a std value img_norm_cfg, default=None (using img_norm_cfg values of each dataset)")
+
+parser.add_argument("--dataset_dir", default='./datasets', type=str, help="train_dataset_dir")
+parser.add_argument("--batchSize", type=int, default=4, help="Training batch sizse")
+parser.add_argument("--patchSize", type=int, default=256, help="Training patch size")
+parser.add_argument("--save", default='./log', type=str, help="Save path of checkpoints")
+parser.add_argument("--resume", default=None, nargs='+', help="Resume from exisiting checkpoints (default: None)")
+parser.add_argument("--pretrained", default=None, nargs='+', help="Load pretrained checkpoints (default: None)")
+parser.add_argument("--nEpochs", type=int, default=1000, help="Number of epochs")
+parser.add_argument("--optimizer_name", default='Adam', type=str, help="optimizer name: Adam, Adagrad, SGD")
+parser.add_argument("--optimizer_settings", default={'lr': 0.001}, type=dict, help="optimizer settings")
+parser.add_argument("--scheduler_name", default='CosineAnnealingLR', type=str, help="scheduler name: MultiStepLR")
+parser.add_argument("--scheduler_settings", default={'eta_min': 1e-5, 'last_epoch': -1}, type=dict,
+                    help="scheduler settings")
+parser.add_argument("--threads", type=int, default=0, help="Number of threads for data loader to use")
+parser.add_argument("--threshold", type=float, default=0.5, help="Threshold for test")
+parser.add_argument("--seed", type=int, default=42, help="Threshold for test")
+
+global opt
+opt = parser.parse_args()
+## Set img_norm_cfg
+if opt.img_norm_cfg_mean != None and opt.img_norm_cfg_std != None:
+    opt.img_norm_cfg = dict()
+    opt.img_norm_cfg['mean'] = opt.img_norm_cfg_mean
+    opt.img_norm_cfg['std'] = opt.img_norm_cfg_std
+
+seed_pytorch(opt.seed)
+
+def weights_init_kaiming(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+    elif classname.find('Linear') != -1:
+        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+    elif classname.find('BatchNorm') != -1:
+        init.normal_(m.weight.data, 1.0, 0.02)
+        init.constant_(m.bias.data, 0.0)
+
+def train():
+    train_set = TrainSetLoader(dataset_dir=opt.dataset_dir, dataset_name=opt.dataset_name, patch_size=opt.patchSize,
+                               img_norm_cfg=opt.img_norm_cfg)
+    train_loader = DataLoader(dataset=train_set, num_workers=opt.threads, batch_size=opt.batchSize, shuffle=True)
+
+    net = Net(model_name=opt.model_name, mode='train').cuda()
+    net.apply(weights_init_kaiming)
+    net.train()
+
+    epoch_state = 0
+    total_loss_list = []
+    total_loss_epoch = []
+
+    if opt.resume:
+        for resume_pth in opt.resume:
+            if opt.dataset_name in resume_pth and opt.model_name in resume_pth:
+                ckpt = torch.load(resume_pth)
+                net.load_state_dict(ckpt['state_dict'])
+                epoch_state = ckpt['epoch']
+                total_loss_list = ckpt['total_loss']
+                for i in range(len(opt.scheduler_settings['step'])):
+                    opt.scheduler_settings['step'][i] = opt.scheduler_settings['step'][i] - ckpt['epoch']
+    if opt.pretrained:
+        for pretrained_pth in opt.pretrained:
+            if opt.dataset_name in pretrained_pth and opt.model_name in pretrained_pth:
+                ckpt = torch.load(resume_pth)
+                net.load_state_dict(ckpt['state_dict'])
+
+    ### Default settings of SCTransNet
+    if opt.optimizer_name == 'Adam':
+        opt.optimizer_settings = {'lr': 0.001}
+        opt.scheduler_name = 'CosineAnnealingLR'
+        opt.scheduler_settings = {'epochs': 1000, 'eta_min': 1e-5, 'last_epoch': -1}
+
+    opt.nEpochs = opt.scheduler_settings['epochs']
+
+    ### Default settings of DNANet
+    if opt.optimizer_name == 'Adagrad':
+        opt.optimizer_settings = {'lr': 0.05}
+        opt.scheduler_name = 'CosineAnnealingLR'
+        opt.scheduler_settings = {'epochs': 1500, 'min_lr': 1e-5}
+        opt.scheduler_settings['epochs'] = opt.nEpochs
+
+    opt.nEpochs = opt.scheduler_settings['epochs']
+
+    optimizer, scheduler = get_optimizer(net, opt.optimizer_name, opt.scheduler_name, opt.optimizer_settings,
+                                         opt.scheduler_settings)
+
+    for idx_epoch in range(epoch_state, opt.nEpochs):
+        for idx_iter, (img, gt_mask) in enumerate(train_loader):
+            img, gt_mask = Variable(img).cuda(), Variable(gt_mask).cuda()
+            if img.shape[0] == 1:
+                continue
+            pred = net.forward(img)
+            loss = net.loss(pred, gt_mask)
+            total_loss_epoch.append(loss.detach().cpu())
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        scheduler.step()
+        if (idx_epoch + 1) % 10 == 0:
+            total_loss_list.append(float(np.array(total_loss_epoch).mean()))
+            print(time.ctime()[4:-5] + ' Epoch---%d, total_loss---%f,'
+                  % (idx_epoch + 1, total_loss_list[-1]))
+            opt.f.write(time.ctime()[4:-5] + ' Epoch---%d, total_loss---%f,\n'
+                        % (idx_epoch + 1, total_loss_list[-1]))
+            total_loss_epoch = []
+
+        if (idx_epoch + 1) % 20 == 0:
+            save_pth = opt.save + '/' + opt.dataset_name + '/' + opt.model_name + '_' + str(idx_epoch + 1) + '.pth.tar'
+            save_checkpoint({
+                'epoch': idx_epoch + 1,
+                'state_dict': net.state_dict(),
+                'total_loss': total_loss_list,
+            }, save_pth)
+            # test(save_pth)
+
+        if (idx_epoch + 1) == opt.nEpochs and (idx_epoch + 1) % 20 != 0:
+            save_pth = opt.save + '/' + opt.dataset_name + '/' + opt.model_name + '_' + str(idx_epoch + 1) + '.pth.tar'
+            save_checkpoint({
+                'epoch': idx_epoch + 1,
+                'state_dict': net.state_dict(),
+                'total_loss': total_loss_list,
+            }, save_pth)
+            # test(save_pth)
+
+
+# def test(save_pth):
+#     test_set = TestSetLoader(opt.dataset_dir, opt.dataset_name, opt.dataset_name, img_norm_cfg=opt.img_norm_cfg)
+#     test_loader = DataLoader(dataset=test_set, num_workers=1, batch_size=1, shuffle=False)
+#
+#     net = Net(model_name=opt.model_name, mode='test').cuda()
+#     ckpt = torch.load(save_pth)
+#     net.load_state_dict(ckpt['state_dict'])
+#     net.eval()
+#
+#     eval_mIoU = mIoU()
+#     eval_PD_FA = PD_FA()
+#     for idx_iter, (img, gt_mask, size, _) in enumerate(test_loader):
+#         img = Variable(img).cuda()
+#         pred = net.forward(img)
+#         pred = pred[:, :, :size[0], :size[1]]
+#         gt_mask = gt_mask[:, :, :size[0], :size[1]]
+#         eval_mIoU.update((pred > opt.threshold).cpu(), gt_mask)
+#         eval_PD_FA.update((pred[0, 0, :, :] > opt.threshold).cpu(), gt_mask[0, 0, :, :], size)
+#
+#     results1 = eval_mIoU.get()
+#     results2 = eval_PD_FA.get()
+#     print("pixAcc, mIoU:\t" + str(results1))
+#     print("PD, FA:\t" + str(results2))
+#     opt.f.write("pixAcc, mIoU:\t" + str(results1) + '\n')
+#     opt.f.write("PD, FA:\t" + str(results2) + '\n')
+
+
+def save_checkpoint(state, save_path):
+    if not os.path.exists(os.path.dirname(save_path)):
+        os.makedirs(os.path.dirname(save_path))
+    torch.save(state, save_path)
+    return save_path
+
+
+if __name__ == '__main__':
+    for dataset_name in opt.dataset_names:
+        opt.dataset_name = dataset_name
+        for model_name in opt.model_names:
+            opt.model_name = model_name
+            if not os.path.exists(opt.save):
+                os.makedirs(opt.save)
+            opt.f = open(opt.save + '/' + opt.dataset_name + '_' + opt.model_name + '_' + (time.ctime()).replace(' ',
+                                                                                                                 '_').replace(
+                ':', '_') + '.txt', 'w')
+            print(opt.dataset_name + '\t' + opt.model_name)
+            train()
+            print('\n')
+            opt.f.close()
