86c4961ef1393f89b89354aa21a8a157acf58cec
我运气超好 <yj20220275@stud.tjut.edu.cn>
2024-06-15 21:32:32 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -2,17 +2,16 @@ import argparse
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from net import Net
-from dataset import *
+from model.dataset import *
 import matplotlib.pyplot as plt
 from metrics import *
 import os
 import time
 from tqdm import tqdm
-
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 parser = argparse.ArgumentParser(description="PyTorch BasicIRSTD Inference without mask")
-#parser.add_argument("--model_names", default=['ACM', 'ALCNet','DNANet', 'ISNet', 'RDIAN', 'ISTDU-Net'], nargs='+', help="model_name: 'ACM', 'ALCNet', 'DNANet', 'ISNet', 'UIUNet', 'RDIAN', 'ISTDU-Net', 'U-Net', 'RISTDnet'")
-parser.add_argument("--model_names", default=['IS_ViMamba'], type=str,nargs="+", help="model_name: 'IS_ViMamba','SCTransNet', 'MSHNet', 'DNANet', 'ISNet', 'UIUNet', 'RDIAN', 'ISTDU-Net', 'U-Net', 'RISTDnet'")
+parser.add_argument("--model_names", default=['ACM', 'ALCNet','DNANet', 'ISNet', 'RDIAN', 'ISTDU-Net'], nargs='+',  
+                    help="model_name: 'ACM', 'ALCNet', 'DNANet', 'ISNet', 'UIUNet', 'RDIAN', 'ISTDU-Net', 'U-Net', 'RISTDnet'")
 parser.add_argument("--pth_dirs", default=None, nargs='+',  help="checkpoint dir, default=None or ['NUDT-SIRST/ACM_400.pth.tar','NUAA-SIRST/ACM_400.pth.tar']")
 parser.add_argument("--dataset_dir", default='./datasets', type=str, help="train_dataset_dir")
 parser.add_argument("--dataset_names", default=['NUAA-SIRST', 'NUDT-SIRST', 'IRSTD-1K'], nargs='+', 
@@ -36,32 +35,10 @@ if opt.img_norm_cfg_mean != None and opt.img_norm_cfg_std != None:
   opt.img_norm_cfg = dict()
   opt.img_norm_cfg['mean'] = opt.img_norm_cfg_mean
   opt.img_norm_cfg['std'] = opt.img_norm_cfg_std
-
-# 定义函数进行图像分割预测并拼接
-def predict_and_concat(net, img):
-    _, _, height, width = img.size()
-
-    m = max(height, width)
-    if m > 2048:
-
-        # 将图像从中间水平分割成左右两半
-        img_left = img[:, :, :, :width // 2]
-        img_right = img[:, :, :, width // 2:]
-
-        # 分别对左右两半进行预测
-        pred_left = net.forward(img_left.cuda())
-        pred_right = net.forward(img_right.cuda())
-
-        # 将左右两半的预测结果拼接起来
-        pred = torch.cat((pred_left, pred_right), dim=3)
-    else:
-        pred = net.forward(img.cuda())
-
-    return pred
   
 def test(): 
     test_set = InferenceSetLoader(opt.dataset_dir, opt.train_dataset_name, opt.test_dataset_name, opt.img_norm_cfg)
-    test_loader = DataLoader(dataset=test_set, num_workers=0, batch_size=1, shuffle=False)
+    test_loader = DataLoader(dataset=test_set, num_workers=1, batch_size=1, shuffle=False)
     
     net = Net(model_name=opt.model_name, mode='test').cuda()
     try:
@@ -71,13 +48,58 @@ def test():
         net.load_state_dict(torch.load(opt.pth_dir, map_location=device)['state_dict'])
     net.eval()
 
+    # with torch.no_grad():
+    #     for idx_iter, (img, size, img_dir) in tqdm(enumerate(test_loader)):
+    #         img = Variable(img).cuda()
+    #         pred = net.forward(img)
+    #         pred = pred[:,:,:size[0],:size[1]]
+
+    max_block_size = (512, 512)
     with torch.no_grad():
         for idx_iter, (img, size, img_dir) in tqdm(enumerate(test_loader)):
-            # img = Variable(img).cuda()
-            # pred = net.forward(img)
-            torch.cuda.empty_cache()
-            pred = predict_and_concat(net, img)
-            pred = pred[:,:,:size[0],:size[1]]        
+            img = Variable(img).cuda()
+            _, _, height, width = img.size()
+
+            # 计算需要填充的尺寸
+            pad_height = (max_block_size[0] - height % max_block_size[0]) % max_block_size[0] # 512 - 832 % 512 = 192
+            pad_width = (max_block_size[1] - width % max_block_size[1]) % max_block_size[1] # 512 - 1088 % 512 = 448
+          
+            # 对图像进行填充
+            # img = F.pad(img, (0, 0, pad_width, pad_height), padding_mode='constant', fill=0) #new version F.pad  (left,top,right,bootom)
+            img = F.pad(img, (0, pad_width, 0, pad_height), mode='constant', value=0)  #old version F.pad  (left,top,right,bootom)
+            _, _, padded_height, padded_width = img.size()
+
+            num_blocks_height = (padded_height + max_block_size[0] - 1) // max_block_size[0]
+            num_blocks_width = (padded_width + max_block_size[1] - 1) // max_block_size[1]
+
+            # 动态分块推理
+            output = torch.zeros_like(img)
+            for i in range(num_blocks_height):
+                for j in range(num_blocks_width):
+                    block_y = i * max_block_size[0]
+                    block_x = j * max_block_size[1]
+                    block_height = min(max_block_size[0], padded_height - block_y)
+                    block_width = min(max_block_size[1], padded_width - block_x)
+
+                    # 确保块的尺寸大于0
+                    if block_height <= 0 or block_width <= 0:
+                        print(f'Skipping block at (i={i}, j={j}) due to zero or negative size: height={block_height}, width={block_width}')
+                        continue
+
+                    block = img[:, :, block_y:block_y + block_height, block_x:block_x + block_width]
+                    
+
+                    try:
+                        pred_block = net.forward(block)
+                    except RuntimeError as e:
+                        print(f'Error processing block at (i={i}, j={j}): {str(e)}')
+                        continue
+
+                    output[:, :, block_y:block_y + block_height, block_x:block_x + block_width] = pred_block
+
+            # 去除填充部分
+            output = output[:,:,:size[0],:size[1]]
+            pred = output     
             ### save img
             if opt.save_img == True:
                 img_save = transforms.ToPILImage()(((pred[0,0,:,:]>opt.threshold).float()).cpu())
